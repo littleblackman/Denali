d61c5dacefe82f9f82236cc074575f7213e52858
littleBlackMan <littleblackman@users.noreply.github.com>
2017-10-25 11:47:58 +0200

    session destroy

--- controller/Logout.php
+++ controller/Logout.php
@@ -1,7 +1,8 @@
 <?php
-session_start();
 
-session_destroy();
+$session = new UserSession();
+$session->destroy();
+
 $_SESSION['flash']['success'] = 'vous etes maintenant deconnecte';
 
 // Inclusion du template
