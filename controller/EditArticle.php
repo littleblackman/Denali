df328bc7521aa3ad4675fa22ebb70dc49e9be322
littleBlackMan <littleblackman@users.noreply.github.com>
2017-10-25 11:18:33 +0200

    indentation

--- controller/EditArticle.php
+++ controller/EditArticle.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
-	if(!empty($_GET['id']) AND (ctype_digit($_GET['id'])))
-	{
-		$idArticle = $_GET['id'];
-		$articleManager = new ArticleManager();
-        $article= $articleManager->readOne($idArticle);
-		include_once 'templates/editArticle.php';
-		exit;
-	} else
-	{
-		header('location: templates/error.php');
-		exit;
-	}
+if(!empty($_GET['id']) AND (ctype_digit($_GET['id'])))
+{
+	$idArticle = $_GET['id'];
+	$articleManager = new ArticleManager();
+$article= $articleManager->readOne($idArticle);
+	include_once 'templates/editArticle.php';
+	exit;
+} else
+{
+	header('location: templates/error.php');
+	exit;
+}
 	
 	
